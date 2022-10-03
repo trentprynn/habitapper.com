32176ba61f7329980cb53063eda38f7fbf6b158b
Trent Prynn <trentprynn@gmail.com>
2022-10-03 16:18:45 -0700

    chore: upgrade dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import SSRProvider from 'react-bootstrap/SSRProvider'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+    Component,
+    pageProps,
+}: AppProps<{
+    session: Session
+}>) {
     return (
         <SSRProvider>
             <SessionProvider session={pageProps.session}>
