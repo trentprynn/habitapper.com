0eb23f889a1bb74ddedce23d3343dfd6d452c65e
Trent Prynn <trentprynn@gmail.com>
2022-12-17 09:48:33 -0700

    fix: update website description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,16 @@ export default function Document() {
         <Html>
             <Head>
                 <meta charSet="utf-8" />
-                <meta name="description" content="feedback loop based habit tracking" />
+                <meta
+                    name="description"
+                    content="HabiTapper is an open source, privacy focused daily habit tracking web application. It's like snapchat streaks but for your daily habits!"
+                />
                 <meta property="og:title" content="HabiTapper" />
                 <meta property="og:image" content="https://habitapper.com/default.png" />
-                <meta property="og:description" content="feedback loop based habit tracking" />
+                <meta
+                    property="og:description"
+                    content="HabiTapper is an open source, privacy focused daily habit tracking web application. It's like snapchat streaks but for your daily habits!"
+                />
                 <link rel="apple-touch-icon" href="https://habitapper.com/logo192.png" />
                 <link rel="manifest" href="https://habitapper.com/manifest.json" />
                 <script async defer data-collect-dnt="true" src="https://sa.habitapper.com/latest.js"></script>
